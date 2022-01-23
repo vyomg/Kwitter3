af3f2216320541078258e5e7d6fe0ad17df7e628
vyomg <83865210+vyomg@users.noreply.github.com>
2022-01-23 14:55:59 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,29 @@
+
+//ADD YOUR FIREBASE LINKS HERE
+
+function addRoom()
+{
+      room_name = document.getElementById("room_name").Value;
+      firebase.database().ref("/").child(room_name).update ({
+            purpose : "adding room name"
+      });
+
+      localStorage.setItem("room_name", room_name);
+
+      window.location = "kwitter_page.html";
+}
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      console.log("Room Name  - " + Room_names);
+      row = "<div id='room_name' class="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+ Room_names +"</div><hr>"
+      document.getElementById("output").innerHTML += row;
+      });});}
+getData();
+
+function redirectToRoomName(name)
+{
+      console.log(name);
+      localStorage.setItem("room_name", name);
+      window.location = "kwitter_page.html";
+}
